68a29871f192c73edce3b661734d2d60d5c079ba
tleot <leolax1980@gmail.com>
2021-04-28 11:23:01 -0400

    tictactoe game works but has bugs. needs testing.

--- learn/codeacademy/tictactoe/tictactoe.cpp
+++ learn/codeacademy/tictactoe/tictactoe.cpp
@@ -3,23 +3,23 @@
 
 int main() { 
 
-    char player = 'X';
+    char player = 'X'; 
     int choice = 0; 
     bool endgame = false;
-    bool gc = false;
+    bool gc, wc = false;
     char grid[3][3] = { {'1', '2', '3'}, {'4', '5', '6'}, {'7', '8', '9'} };
 
     while (endgame != true) {
 
-     //   system("clear");
         board(grid);
-        char gc = gridCheck(player, userInput(choice), grid);
-
-        cout << endgame << " - " << gc << " Player " << player << endl;
-        player = gc;
-        cout << endgame << " - " << gc << " Player " << player << endl;
-
-        winCheck(player, grid);
+        gc = gridCheck(player, userInput(choice), grid);
+        wc = winCheck(player, grid);
+        cout << gc << " - " << wc << endl;
+        
+        if (wc == 1)
+            endgame = true;
+        else if (wc == 0 && gc == 1)
+            player = chngPlayer(player);
 
     } 
         
--- learn/codeacademy/tictactoe/tictactoe_functions.cpp
+++ learn/codeacademy/tictactoe/tictactoe_functions.cpp
@@ -28,139 +28,118 @@ char board(char grid[][3]) {
 // function checks if qaudrant is taken, if not will set the qaudtrant
 // parameters are the player symbol, player choice,  
 // and the tictactoe array 
-char gridCheck(char player, int choice, char grid[][3]) 
+bool gridCheck(char player, int choice, char grid[][3]) 
 {
-    char plyr = player;
 
     switch (choice)
     {
     case 1:
         if (grid[0][0] == 'X' || grid[0][0] == 'O') {
-            cout << "The QUAD is taken. Input your choice again." << endl;
+            cout << "Sorry Player " << player << " The QUAD is taken. Input your choice again." << endl;
+            return false;
             break;
         }
         else {
             grid[0][0] = player;
-
-            if (plyr == 'X')
-                plyr = 'O';
-            else 
-                plyr = 'X';
+            return true;
             break;
             }
     case 2:
         if (grid[0][1] == 'X' || grid[0][1] == 'O') {
-            cout << "The QUAD is taken. Input your choice again." << endl;
+            cout << "Sorry Player " << player << " The QUAD is taken. Input your choice again." << endl;
+            return false;
             break;
         }
         else {
             grid[0][1] = player; 
-            if (plyr == 'X')
-                plyr = 'O';
-            else 
-                plyr = 'X';
-            break;
+            return true;
             } 
     case 3:
         if (grid[0][2] == 'X' || grid[0][2] == 'O') {
-            cout << "The QUAD is taken. Input your choice again." << endl;
+            cout << "Sorry Player " << player << " The QUAD is taken. Input your choice again." << endl;
+            return false;
             break;
         }
         else {
             grid[0][2] = player; 
-            if (plyr == 'X')
-                plyr = 'O';
-            else 
-                plyr = 'X';
-            break;
+            return true;
             }
     case 4:
         if (grid[1][0] == 'X' || grid[1][0] == 'O') {
-            cout << "The QUAD is taken. Input your choice again." << endl;
+            cout << "Sorry Player " << player << " The QUAD is taken. Input your choice again." << endl;
+            return false; 
             break;
         }
         else {
             grid[1][0] = player; 
-            if (plyr == 'X')
-                plyr = 'O';
-            else 
-                plyr = 'X';
+            return true;
             break;
         }
     case 5:
         if (grid[1][1] == 'X' || grid[1][1] == 'O') {
-            cout << "The QUAD is taken. Input your choice again." << endl;
+            cout << "Sorry Player " << player << " The QUAD is taken. Input your choice again." << endl;
+            return false;
             break;
         }
         else {
             grid[1][1] = player; 
-            if (plyr == 'X')
-                plyr = 'O';
-            else 
-                plyr = 'X';
+            return true;
             break;
         }
     case 6:
         if (grid[1][2] == 'X' || grid[1][2] == 'O') {
-            cout << "The QUAD is taken. Input your choice again." << endl;
+            cout << "Sorry Player " << player << " The QUAD is taken. Input your choice again." << endl;
+            return false;
             break;
         }
         else {
             grid[1][2] = player; 
-            if (plyr == 'X')
-                plyr = 'O';
-            else 
-                plyr = 'X';
+            return true;
             break;
         }
     case 7:
         if (grid[2][0] == 'X' || grid[2][0] == 'O') {
-            cout << "The QUAD is taken. Input your choice again." << endl;
+            cout << "Sorry Player " << player << " The QUAD is taken. Input your choice again." << endl;
+            return false;
             break;
         }
         else {
             grid[2][0] = player; 
-            if (plyr == 'X')
-                plyr = 'O';
-            else 
-                plyr = 'X';
+            return true;
             break;
         }
     case 8:
         if (grid[2][1] == 'X' || grid[2][1] == 'O') {
-            cout << "The QUAD is taken. Input your choice again." << endl;
+            cout << "Sorry Player " << player << " The QUAD is taken. Input your choice again." << endl;
+            return false;
             break;
         }
         else {
             grid[2][1] = player; 
-            if (plyr == 'X')
-                plyr = 'O';
-            else 
-                plyr = 'X';
+            return true;
             break;
         }
     case 9:
         if (grid[2][2] == 'X' || grid[2][2] == 'O') {
-            cout << "The QUAD is taken. Input your choice again." << endl;
+            cout << "Sorry Player " << player << " The QUAD is taken. Input your choice again." << endl;
+            return false;
             break;
         }
         else {
             grid[2][2] = player; 
-            if (plyr == 'X')
-                plyr = 'O';
-            else 
-                plyr = 'X';
+            return true;
             break; 
         }
 
     default:
         if (choice > 9 || choice == 0) {
             cout << "Your number is not good. Try again with Input [1-9] Player ..." << endl;
+            return false;
             break;
         }
     
     }
-    return plyr;
+    return false;
 }
 
 // function is designed to ask user for choice and return this choice
@@ -198,11 +177,46 @@ char chngPlayer(char player) {
 // function to check if the player has won.  
 bool winCheck(char player, char grid[][3]) {
 
-    int i, j;
+    // win row 1
     if (grid[0][0] == player && grid[0][1] == player && grid[0][2] == player) {
         cout << "Congrats! Player " << player << ", YOU WIN!!" << endl;
         return true;
         }
+    // win row 2
+    else if (grid[1][0] == player && grid[1][1] == player && grid[1][2] == player) {
+        cout << "Congrats! Player " << player << ", YOU WIN!!" << endl;
+        return true;
+        }
+    // win row 3
+    else if (grid[2][0] == player && grid[2][1] == player && grid[2][2] == player) {
+        cout << "Congrats! Player " << player << ", YOU WIN!!" << endl;
+        return true;
+    }
+    // win diag - l to r
+    else if (grid[0][0] == player && grid[1][1] == player && grid[2][2] == player) {
+        cout << "Congrats! Player " << player << ", YOU WIN!!" << endl;
+        return true;
+        }
+    // win diag - r to l 
+    else if (grid[0][2] == player && grid[1][1] == player && grid[2][0] == player) {
+        cout << "Congrats! Player " << player << ", YOU WIN!!" << endl;
+        return true;
+        }
+    // win col 1
+    else if (grid[0][0] == player && grid[1][0] == player && grid[2][0] == player) {
+        cout << "Congrats! Player " << player << ", YOU WIN!!" << endl;
+        return true;
+        }
+    // win col 2
+    else if (grid[0][1] == player && grid[1][1] == player && grid[2][1] == player) {
+        cout << "Congrats! Player " << player << ", YOU WIN!!" << endl;
+        return true;
+    }
+    // win col 3
+    else if (grid[0][2] == player && grid[1][2] == player && grid[2][2] == player) {
+        cout << "Congrats! Player " << player << ", YOU WIN!!" << endl;
+        return true;
+    }
 
     else
         return false;
