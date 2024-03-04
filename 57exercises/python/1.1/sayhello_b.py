fd80637f94c2fca511a6bd5235a98db07cc51386
mehome <leolax1980@gmail.com>
2024-03-04 16:46:53 -0500

    completed test.py file to run mutliple test cases

---
+++ 57exercises/python/1.1/sayhello_b.py
@@ -0,0 +1,7 @@
+'''Exercise 1 - create a program that prompts for your name and prints a greeting using your name.'''
+def main():
+    print(f"What is you name?")
+    print(f"Hello, {input()}, nice to meet you!")
+
+if __name__ == "__main__":
+    main()
--- 57exercises/python/1.1/test.py
+++ 57exercises/python/1.1/test.py
@@ -1,14 +1,21 @@
 import unittest
 from unittest.mock import patch
-from io import StringIO
-import sayhello
+import sayhello, sayhello_b
 
 class TestSayHello(unittest.TestCase):
-    @patch('builtins.input', return_value='Tony')
+    @patch('builtins.input')
     @patch('builtins.print')
     def test_main(self, mock_print, mock_input):
-        sayhello.main()
-        mock_print.assert_called_with("Hello, Tony, nice to meet you!")
+        # list of test cases
+        test_cases = [
+            (sayhello,"123$%% 23432 #$#1", "Hello, , nice to meet you!"),
+            (sayhello_b, "Tony", "Hello, Tony, nice to meet you!")
+        ]
+        for module, input_value, expected_output in test_cases:
+            with self.subTest(input=input_value, output=expected_output):
+                mock_input.return_value = input_value
+                module.main()
+                mock_print.assert_called_with(expected_output)
 
 if __name__ == "__main__":
     unittest.main()
